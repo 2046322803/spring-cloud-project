8743bb9702e99bb1b382f0e7e53788304d7f34e1
yuanjun.zou <yuanjun.zou@zkteco.com>
2019-03-28 14:01:20 +0800

    code commit

--- spring-cloud-netflix/spring-cloud-netflix-zuul/spring-cloud-netflix-zuul-service/src/main/java/com/zyj/spring/cloud/ZuulConfiguration.java
+++ spring-cloud-netflix/spring-cloud-netflix-zuul/spring-cloud-netflix-zuul-service/src/main/java/com/zyj/spring/cloud/ZuulConfiguration.java
@@ -3,17 +3,33 @@ package com.zyj.spring.cloud;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ZuulConfiguration {
 
+	/**
+	 * 调用/routes接口来获取当前网关上的路由规则
+	 * 调用/refresh接口来刷新配置信息
+	 * @return
+	 */
 	@Bean
-	@RefreshScope
+	@RefreshScope //将zuul的配置内容动态化
 	@ConfigurationProperties("zuul")
 	public ZuulProperties zuulProperties() {
 		return new ZuulProperties();
 	}
-	
+
+	/**
+	 * 自定义路由映射规则
+	 * 
+	 * @return
+	 */
+	@Bean
+	public PatternServiceRouteMapper serviceRouteMapper() {
+		return new PatternServiceRouteMapper("(?<name>^.+)-(?<version>v.+$)", "${version}/${name}");
+	}
+
 }
--- spring-cloud-netflix/spring-cloud-netflix-zuul/spring-cloud-netflix-zuul-service/src/main/java/com/zyj/spring/cloud/ZuulServiceApplication.java
+++ spring-cloud-netflix/spring-cloud-netflix-zuul/spring-cloud-netflix-zuul-service/src/main/java/com/zyj/spring/cloud/ZuulServiceApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
-import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @EnableEurekaClient
@@ -16,16 +14,6 @@ public class ZuulServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulServiceApplication.class, args);
-	}
-
-	/**
-	 * 自定义路由映射规则
-	 * 
-	 * @return
-	 */
-	@Bean
-	public PatternServiceRouteMapper serviceRouteMapper() {
-		return new PatternServiceRouteMapper("(?<name>^.+)-(?<version>v.+$)", "${version}/${name}");
 	}
 
 }
